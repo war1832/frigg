c6d4d4ce3a26d681223cf6bd8219f9203ae0b30b
War <war.v8sf@gmail.com>
2015-03-12 17:58:52 -0300

    add comments

--- app/models/post.rb~
+++ app/models/post.rb~
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   validates :title, :body, presence: true
+  has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :comments, :only => [:create]
+  end
   resources :blog
   
   get 'blog/index'
