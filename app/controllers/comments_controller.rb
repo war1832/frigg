28a842185d8ae70d1c4e9b17bcf3ad5d4a94e2d1
War <war.v8sf@gmail.com>
2015-03-13 11:11:00 -0300

    add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,11 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create!(params[:comment])
-    redirect @post
+    @comment = @post.comments.create(comment_params)
+    redirect_to @post
+  end
+  private
+  def comment_params
+    params[:comment].permit(:body)
   end
 end
