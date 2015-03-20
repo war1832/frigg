22f09f9759b1f7980d425dd476c86168f4883251
Ezequiel Echevarria <war.v8sf@gmail.com>
2015-03-20 12:04:32 -0300

    fix post view controller

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
+  def show_link post
+    content_tag :div do
+        concat link_to 'Read More', blog_post_path(:id =>post,
+                                          :blog_id => post.blog.name),
+                                          :class =>'btn-xs btn-primary link-post'
+      if current_user == post.user
+        concat link_to 'Edit', edit_blog_post_path(:id => post.id,
+                                            :blog_id => post.blog.name),
+                        :class =>'btn-xs btn-primary link-post'
+        concat link_to 'Destroy', blog_post_path(:id => post.id,
+                                          :blog_id => post.blog.name),
+                           :class =>'btn-xs btn-primary link-post',
+                           method: :delete, data: { confirm: 'Are you sure?' }
+      end
+    end
+  end
 end
