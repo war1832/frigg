3afc0556c8e5f237d999f7facfa24f9f2688a4e0
Ezequiel Echevarria <war.v8sf@gmail.com>
2015-03-20 12:32:55 -0300

    add 404 to blog controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,6 +12,7 @@ class BlogsController < ApplicationController
   # GET /blogs/1.json
   def show
     @blog = Blog.find(params[:id])
+    render 'blog_not_found' unless @blog
   end
 
   # GET /blogs/new
