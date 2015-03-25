6b68fd37d3a3a45ba3e68686e9db4182b1504761
Ezequiel Echevarria <war.v8sf@gmail.com>
2015-03-25 12:50:32 -0300

    add post_path

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,18 +1,16 @@
 module PostsHelper
   def show_link post
     content_tag :div do
-        concat link_to 'Read More', blog_post_path(:id =>post,
-                                          :blog_id => post.blog.name),
-                                          :class =>'btn-xs btn-primary link-post'
+        concat link_to 'Read More', post, :class =>'btn-xs btn-primary link-post'
       if current_user == post.user
-        concat link_to 'Edit', edit_blog_post_path(:id => post.id,
-                                            :blog_id => post.blog.name),
-                        :class =>'btn-xs btn-primary link-post'
-        concat link_to 'Destroy', blog_post_path(:id => post.id,
-                                          :blog_id => post.blog.name),
-                           :class =>'btn-xs btn-primary link-post',
+        concat link_to 'Edit', post, :class =>'btn-xs btn-primary link-post'
+        concat link_to 'Destroy', post, :class =>'btn-xs btn-primary link-post',
                            method: :delete, data: { confirm: 'Are you sure?' }
       end
     end
+  end
+  
+  def post_path post
+    url_for [post.blog, post]
   end
 end
