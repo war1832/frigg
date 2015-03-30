e4d6aa25a53f0ad621cfe56421c836a91a325a61
war1832 <war.v8sf@gmail.com>
2015-03-30 12:11:03 -0300

    add beforce action

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    subject { create :user }
- 
-  it "sends an email" do
-    expect { subject.send_instructions }.to change { ActionMailer::Base.deliveries.count }.by(1)
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
