f1e9c60cf8c4d24be204251218e8032b0c851403
war1832 <war.v8sf@gmail.com>
2015-04-01 16:34:12 -0300

    fix deploy ckeditor

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,8 @@ module Frigg
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     config.autoload_paths += %W(#{config.root}/lib)
   end
