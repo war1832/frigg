0cf26ec80832bf33a7f2f9f0f09571d671867a4e
war1832 <war.v8sf@gmail.com>
2015-04-06 14:07:32 -0300

    fix show_link method

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,8 +3,8 @@ module PostsHelper
     content_tag :div do
         concat link_to 'Read More', post, :class =>'btn-xs btn-primary link-post'
       if current_user == post.user
-        concat link_to 'Edit', post, :class =>'btn-xs btn-primary link-post'
-        concat link_to 'Destroy', post, :class =>'btn-xs btn-primary link-post',
+        concat link_to 'Edit', edit_blog_post_path(post.blog, post), :class =>'btn-xs btn-primary link-post'
+        concat link_to 'Destroy', blog_post_path(post.blog, post), :class =>'btn-xs btn-primary link-post',
                            method: :delete, data: { confirm: 'Are you sure?' }
       end
     end
