f3bfe891a6225800f4fc75492645dcea64b78095
war1832 <war.v8sf@gmail.com>
2015-04-07 11:55:38 -0300

    admin role users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331175019) do
+ActiveRecord::Schema.define(version: 20150407145310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 20150331175019) do
     t.text     "bio"
     t.string   "provider"
     t.string   "uid"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
