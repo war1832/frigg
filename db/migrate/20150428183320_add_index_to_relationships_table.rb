f86c2f6968063977ebbd2a4a27e00ec2f32907e4
war1832 <war.v8sf@gmail.com>
2015-04-28 16:11:13 -0300

    follower migration

---
+++ db/migrate/20150428183320_add_index_to_relationships_table.rb
@@ -0,0 +1,7 @@
+class AddIndexToRelationshipsTable < ActiveRecord::Migration
+  def change
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true
+  end
+end
